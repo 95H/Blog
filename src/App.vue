ad3fde102c479d482290c5a154e1f9df5f4b635b
guoxiaohuan <1069138153@qq.com>
2019-11-17 22:53:20 +0800

    bg

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang='scss' scoped>
+#app{
+  width: 100%;
+  height: 100%;
+  background: url('./assets/page-bg-3.jpg');
+  background-repeat:no-repeat;
+  background-size: 100% 100%;
 }
 </style>
---
+++ src/view/first/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+      
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+   .container{
+      width: 100%;
+      height: 100%;
+
+   } 
+
+
+</style>
